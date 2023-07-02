0b3bfec18b4fbc92cfd431c8ccc3040be18e32f4
Swadeb-Pzero <swadebsharmaphero88@gmail.com>
2023-07-02 22:18:47 +0600

    added project files

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT||5000;
+const cors = require('cors');
+
+const categories = require('./data/categories.json');
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Dragon Server is Running')
+  })
+
+
+  app.get('/categories', (req, res) =>{
+    res.send(categories);
+  })
+
+
+  app.listen(port, () => {
+    console.log(`Dragon Api is Running on port ${port}`)
+  })
